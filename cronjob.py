328fe277a3cf7715195c3f6e7402ef35150acf34
Saurav Dosi <45575805+sauravdosi@users.noreply.github.com>
2021-05-17 23:16:42 +0530

    Update cronjob.py

--- cronjob.py
+++ cronjob.py
@@ -10,24 +10,31 @@ delete_day = delete_day.strftime("%Y/%m/%d")
 
 print("All the images captured on " + str(delete_day) + " will be erased.")
 
-retention_dir = config.get_config("out_path") + "/" + delete_day
+delete_dir = config.get_config("out_path") + "/" + delete_day
+
+print(delete_dir)
 
 totalFiles = 0
 totalDir = 0
 
-for base, dirs, files in os.walk(retention_dir):
-    print('Counting files: ', base)
-   
+print("Counting files in: ", delete_dir)
+
+for base, dirs, files in os.walk(delete_dir):
+    
     for Files in files:
         totalFiles += 1
 
-print('Total number of files',totalFiles)
+print(totalFiles, " files will be deleted.")
 
-if os.path.exists(delete_path):
+
+if os.path.exists(delete_dir):
 
 	try:
-# 		shutil.rmtree(delete_path)
-		print("This directory is deleted.")
+		shutil.rmtree(delete_dir)
+		print("The directory is erased.")
+
+		with open('trash_log.txt', 'a+') as file:
+			file.write("On " + str(datetime.datetime.now().strftime("%Y/%m/%d-%H:%M:%S:%f")) + ", " + str(totalFiles) + " files were deleted from " + str(delete_dir) + ".\n")
 
 	except:
 		print("No such path exists.")
