f8e456540d4ba0b983071eda3ce1430c1370f90a
Saurav Dosi <45575805+sauravdosi@users.noreply.github.com>
2021-05-17 22:53:07 +0530

    Update cronjob.py

--- cronjob.py
+++ cronjob.py
@@ -1,5 +1,3 @@
-# with open('timestamps.txt', 'a+') as file:
-	# file.write(str(datetime.now()))
 import os, datetime
 import shutil
 from config import ConfigTF
@@ -8,24 +6,36 @@ config = ConfigTF()
 retention_period = int(config.get_config("retention_period"))
 
 delete_day = datetime.datetime.now() - datetime.timedelta(retention_period)
-delete_month = delete_day.strftime("%Y/%m")
-delete_day = delete_day.strftime("%d")
-print(delete_day)
+delete_day = delete_day.strftime("%Y/%m/%d")
 
-retention_dir = config.get_config("out_path") + "/" + delete_month
+print("All the images captured on " + str(delete_day) + " will be erased.")
 
-list_dir = os.listdir(retention_dir)
+retention_dir = config.get_config("out_path") + "/" + delete_day
 
-delete_path = retention_dir + "/" + delete_day
+# list_dir = os.listdir(retention_dir)
 
-l2 = os.walk(delete_path)
-print()
+# delete_path = retention_dir + "/" + delete_day
+
+# l2 = os.walk(delete_path)
+# print()
 # folders = os.listdir(violations_dir + str(datetime.today().strftime("%Y")))
+
+totalFiles = 0
+totalDir = 0
+
+for base, dirs, files in os.walk(retention_dir):
+    print('Counting files: ', base)
+   
+    for Files in files:
+        totalFiles += 1
+
+print('Total number of files',totalFiles)
 
 if os.path.exists(delete_path):
 
 	try:
-		shutil.rmtree(delete_path)
+# 		shutil.rmtree(delete_path)
+		print("This directory is deleted.")
 
 	except:
 		print("No such path exists.")
